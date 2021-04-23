f6d9d602663c87e6421a56aaae1338939e557b54
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-04-23 22:29:13 +0200

    added icons and content to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="header__left">
-                <img className="header__leftLogo" 
-                src="https://pngimg.com/uploads/twitch/twitch_PNG6.png" 
-                alt="twitch logo"/>
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img className="header__leftLogo" src="https://pngimg.com/uploads/twitch/twitch_PNG6.png" alt="twitch logo" />
 
-                <h2>Following</h2>
-                <h2>Browse</h2>
-                <div className="header__verticalLine"></div>
-            </div>
+        <h2>Following</h2>
+        <h2>Browse</h2>
+        <div className="header__verticalLine"></div>
+        <h2>Exports</h2>
+        <h2>Music</h2>
+        <i class="fas fa-ellipsis-h"></i>
+      </div>
 
-            <div className="header__center">
-
-            </div>
-
-            <div className="header__right">
-
-            </div>
+      <div className="header__center">
+        <input type="text" placeholder="Search" />
+        <div className="header_centerLogoContainer">
+          <i class="fas fa-search"></i>
         </div>
-    )
-}
+      </div>
 
-export default Header
+      <div className="header__right">
+        <div className="header__rightContainer">
+        <i class="fas fa-crown"></i>
+        <i class="fas fa-inbox"></i>
+        <i class="fas fa-comment-alt"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
