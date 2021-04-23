93eb8af080ff2838815afb2d2c3650f38a8f3c0e
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-04-23 22:10:59 +0200

    added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     // BEM
     <div className="app">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img className="header__leftLogo" 
+                src="https://pngimg.com/uploads/twitch/twitch_PNG6.png" 
+                alt="twitch logo"/>
+
+                <h2>Following</h2>
+                <h2>Browse</h2>
+                <div className="header__verticalLine"></div>
+            </div>
+
+            <div className="header__center">
+
+            </div>
+
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
